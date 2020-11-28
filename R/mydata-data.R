5150cb53944663a185c2cf4f591a1707c95dcd6b
Park-chongnam <s8036@koica.go.kr>
2020-11-28 16:37:54 +0900

    test_crs
    test

---
+++ R/mydata-data.R
@@ -0,0 +1,13 @@
+#' OECD CRS data
+#'
+#' Data from 'stats.oecd.org'
+#'
+#' @docType data
+#'
+#' @usage data(mydata)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' ## Requires font for korean text
+#' data(mydata)
